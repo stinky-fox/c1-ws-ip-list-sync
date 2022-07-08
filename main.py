060cd891c018db68f2d08435e6a0dcda3a8e130e
Stinky Fox <nomail@example.com>
2022-07-08 12:58:14 +0000

    initial push

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import requests
+import json
+import os
+
+
+'''
+Function that acts as a manager to other functions.
+'''
+
+def elCapitan():
+    pass
+
+def abusedbApicaller():
+    pass
+
+def buildList():
+    pass
+
+def c1wsApiCaller():
+    pass
+
